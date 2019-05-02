9c69bb6d85306f9f25e7e74a27553859159e3545
George Varghese M <georgevarghese185@yahoo.com>
2019-05-03 00:10:28 +0530

    Init

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+	module: {
+		rules: [
+			{
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      }
+		]
+	},
+
+	output: {
+		chunkFilename: '[name].[chunkhash].js',
+		filename: '[name].js'
+	},
+
+	mode: 'development',
+	plugins: [
+		new UglifyJSPlugin(),
+		new VueLoaderPlugin()
+	],
+
+	devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true
+  },
+
+	optimization: {
+		splitChunks: {
+			cacheGroups: {
+				vendors: {
+					priority: -10,
+					test: /[\\/]node_modules[\\/]/
+				}
+			},
+
+			chunks: 'async',
+			minChunks: 1,
+			minSize: 30000,
+			name: true
+		}
+	}
+};
