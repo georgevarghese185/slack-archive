cf7c7455078403dad33ffb14a09b2cd77aca0a2e
George Varghese M <george@georgevm.com>
2020-02-06 16:51:19 +0530

    Implement GET:/v1/login/auth-url

--- src/backend/api/login.js
+++ src/backend/api/login.js
@@ -1,4 +1,17 @@
+const constants = require('../../constants');
+const Response = require('../../types/Response');
 const getAuthUrl = () => {
+    const body = {
+        url: constants.slack.oauthUrl,
+        parameters: {
+            client_id: constants.slack.clientId,
+            scope: constants.slack.scope.publicMessages,
+            redirect_uri: constants.slack.oauthRedirectUrl,
+            team: constants.slack.teamId
+        }
+    };
+
+    return new Response({ status: 200, body });
 }
 
 module.exports = {
