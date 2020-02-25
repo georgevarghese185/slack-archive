9997fa7c32a9d239deed60fef9b6a9591136a9ec
George Varghese M <george@georgevm.com>
2020-02-25 11:58:35 +0530

    Add test for GET:/v1/conversations/:id

--- test/api/conversations.test.js
+++ test/api/conversations.test.js
@@ -1,6 +1,7 @@
 const Conversations = require('../../src/backend/models/Conversations');
 const decache = require('decache');
 const expect = require('chai').expect;
+const Request = require('../../src/types/Request');
 
 module.exports = () => {
     let api;
@@ -30,5 +31,29 @@ module.exports = () => {
             expect(response.status).to.equal(200);
             expect(response.body).to.deep.equal({ conversations: conversationList });
         });
+    });
+
+    describe('GET:/v1/conversations/:id', () => {
+        it('get conversation info', async () => {
+            const conversationId = 'C1';
+            const conversationObj = {};
+            const request = new Request({
+                parameters: { id: conversationId }
+            });
+
+            class TestConversations extends Conversations {
+                async get(id) {
+                    expect(id).to.equal(conversationId);
+                    return conversationObj;
+                }
+            }
+            const conversations = new TestConversations();
+            const models = { conversations };
+
+            const response = await api['GET:/v1/conversations/:id'](request, models);
+
+            expect(response.status).to.equal(200);
+            expect(response.body).to.deep.equal(conversationObj);
+        })
     })
 }
