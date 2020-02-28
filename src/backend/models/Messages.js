4d67c306eb9256ac2a6d05d72b5b8fa4613b3ed5
George Varghese M <george@georgevm.com>
2020-02-28 14:51:14 +0530

    Add doc

--- src/backend/models/Messages.js
+++ src/backend/models/Messages.js
@@ -1,4 +1,27 @@
+/**
+ * Interface of a data model for backed up message from the Slack workspace.
+ * This class must be extended and implemented
+ *
+ * @interface Messages
+ */
 class Messages {
+    /**
+     *
+     * @param {Object|null} from - Fetch messages from this time stamp onwards. Structure
+     * should be { value, inclusive } where `value` is a Slack ts string and `inclusive`
+     * can be `true` (include messages with a ts equal to `value`) or `false` (don't include `value`)
+     * @param {Object|null} to - Fetch messages up to this time stamp onwards. Structure
+     * should be { value, inclusive } where `value` is a Slack ts string and `inclusive`
+     * can be `true` (include messages with a ts equal to `value`) or `false` (don't include `value`)
+     * @param {string|null} conversationId - Only fetch messages from this Slack conversation
+     * @param {boolean} postsOnly - Only fetch messages that were posted directly to a
+     * conversation (no messages that were only replies to a thread)
+     * @param {string|null} threadTs - The `ts` of the parent message of a thread. Only messages
+     * from this thread will be returned
+     * @param {number} limit - Limit maximum number of messages to be returned (default: 100)
+     * @returns {Array<Object>} List of messages. Each message object will have the type of
+     * a Slack [message object]{@link https://api.slack.com/events/message}
+     */
     get(from, to, conversationId, postsOnly, threadTs, limit) {
         throw new Error('Not implemented');
     }
