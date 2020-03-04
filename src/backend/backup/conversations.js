d63c85005ddd1dd2a3530d91b370553bf03a146e
George Varghese M <george@georgevm.com>
2020-03-04 12:22:09 +0530

    Implement conversations backup

--- src/backend/backup/conversations.js
+++ src/backend/backup/conversations.js
@@ -1,6 +1,21 @@
+const axios = require('axios');
+const constants = require('../../constants');
 
-const backupConversations = async () => {
+const backupConversations = async (backupId, token, models) => {
+    await models.backups.setStatus(backupId, 'COLLECTING_INFO');
+    const axiosInstance = axios.create({ baseURL: constants.slack.apiBaseUrl });
 
+    const response = await axiosInstance.post('/conversations.list', {}, {
+        headers: {
+            'Authorization': `Bearer ${token.accessToken}`
+        }
+    });
+
+    const conversations = response.data.channels;
+
+    for(const conversation of conversations) {
+        await models.conversations.add(conversation.id, conversation.name, conversation);
+    }
 }
 
 
