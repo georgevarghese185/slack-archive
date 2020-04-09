2bccfcf8c58dae2bcb96769e13a3aab3ad1019a0
George Varghese M <george@georgevm.com>
2020-04-09 15:10:39 +0530

    Implement backupMembers()

--- src/backend/backup/members.js
+++ src/backend/backup/members.js
@@ -1,6 +1,21 @@
+const axios = require('axios');
+const constants = require('../../constants');
 
-const backupMembers = async () => {
+const backupMembers = async (backupId, token, models) => {
+    await models.backups.setStatus(backupId, 'COLLECTING_INFO');
+    const axiosInstance = axios.create({ baseURL: constants.slack.apiBaseUrl });
 
+    const config = {
+        headers: {
+            'Authorization': `Bearer ${token.accessToken}`
+        }
+    }
+
+    const response = await axiosInstance.get('/users.list', config);
+
+    const members = response.data.members;
+
+    models.members.add(members);
 }
 
 
