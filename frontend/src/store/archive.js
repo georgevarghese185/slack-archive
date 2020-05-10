99a9f9a021450001dcbbf50305b6f64c2b99f97a
George Varghese M <george@georgevm.com>
2020-05-10 22:22:26 +0530

    Add store module for message archive

---
+++ frontend/src/store/archive.js
@@ -0,0 +1,65 @@
+import MessagesRemote from '../models/remote/Messages'
+
+const MESSAGE_API_LIMIT = 50
+
+const models = {
+  messages: {
+    remote: new MessagesRemote({ baseUrl: process.env.VUE_APP_API_BASE_URL })
+  }
+}
+
+export default {
+  state: () => ({
+    messages: {
+      list: null,
+      hasOlder: false, // are there more older messages available to fetch
+      hasNewer: false // are there more newer messages available to fetch
+    }
+  }),
+  mutations: {
+    updateMessages (state, { conversationId, focusDate, messages, hasOlder, hasNewer }) {
+      state.messages = {
+        conversationId,
+        focusDate,
+        list: messages,
+        hasNewer,
+        hasOlder
+      }
+    }
+  },
+  actions: {
+    async getMessages (context, { conversationId, ts }) {
+      try {
+        const [olderMessages, newerMessages] = await Promise.all([
+          models.messages.remote.get(
+            null,
+            { inclusive: false, value: ts },
+            conversationId,
+            true,
+            null,
+            MESSAGE_API_LIMIT
+          ),
+          models.messages.remote.get(
+            { inclusive: true, value: ts },
+            null,
+            conversationId,
+            true,
+            null,
+            MESSAGE_API_LIMIT
+          )
+        ])
+
+        context.commit('updateMessages', {
+          conversationId,
+          focusDate: ts,
+          messages: olderMessages.concat(newerMessages),
+          hasOlder: olderMessages.length >= MESSAGE_API_LIMIT,
+          hasNewer: newerMessages.length >= MESSAGE_API_LIMIT
+        })
+      } catch (e) {
+        console.error(e)
+        // TODO handle error
+      }
+    }
+  }
+}
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import archive from './archive'
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
+  modules: {
+    archive
+  }
 })
 
 export default store
