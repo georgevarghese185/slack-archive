6b54d937ea3db5ea74cdee47ccb8b299f104fdd9
George Varghese M <george@georgevm.com>
2020-05-31 16:57:33 +0530

    Use interceptor to attach cookie in backup test

--- backend/test/integration/backup/backup.spec.js
+++ backend/test/integration/backup/backup.spec.js
@@ -3,14 +3,21 @@ const { expect } = require('chai');
 const { login } = require('../login/loginHelper');
 
 describe('Backup', () => {
-    const axiosInstance = axios.create({ baseURL: `http://localhost:${process.env.PORT}` })
+    const axiosInstance = axios.create({ baseURL: `http://localhost:${process.env.PORT}` });
+    let loginCookie = null;
+
+    axiosInstance.interceptors.request.use((config) => {
+        if (loginCookie != null) {
+            config.headers.Cookie = loginCookie;
+        }
+
+        return config;
+    });
 
     it('backup stats should be empty', async () => {
-        const { loginCookie } = await login();
+        loginCookie = (await login()).loginCookie;
 
-        const { data: stats } = await axiosInstance.get('/v1/backup/stats', {
-            headers: { 'Cookie': loginCookie }
-        });
+        const { data: stats } = await axiosInstance.get('/v1/backup/stats');
 
         expect(stats).to.deep.equal({
             messages: 0,
