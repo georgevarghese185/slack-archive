7854d1db88a0ccca270077d746d55e9315d8f0ce
George Varghese M <george@georgevm.com>
2020-05-31 14:50:19 +0530

    Remove jsdoc ref to unused properties

--- backend/src/models/Backups.js
+++ backend/src/models/Backups.js
@@ -14,10 +14,6 @@
  * @property {string} createdBy - The Slack user ID of the user who started this task
  * @property {number|null} endedAt - The the time the task was ended at (in UNIX time milliseconds)
  * @property {BackupStatus} status - The current status of this task
- * @property {string|null} firstMessage - `ts` of the first backed up message. Can be `null` till
- * the first message is backed up
- * @property {string|null} lastMessage - `ts` of the last backed up message. Can be `null` till
- * the last message is backed up
  * @property {number} messagesBackedUp - Number of messages that have been backed up so far
  * @property {string|null} currentConversation - ID of the Slack conversation that is currently
  * being backed up. Can be null until the backup of the first Conversation starts
