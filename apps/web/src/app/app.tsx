c29e84ac8a1bd5d6fba2402ced13b44aa14ea5f5
George Varghese M <george@georgevm.com>
2022-08-13 16:19:16 +0530

    Add react router

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -2,6 +2,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { Header } from './header';
 import { ThemeProvider } from './theme-provider';
 import { WelcomePage } from '../home';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 const App = () => {
   return (
@@ -9,7 +10,13 @@ const App = () => {
       <ThemeProvider>
         <CssBaseline />
         <Header />
-        <WelcomePage />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/sign-in" element={<WelcomePage />} />
+            <Route path="/" element={<Navigate to="/sign-in" replace />} />
+            <Route path="*" element={<>{'Not Found'}</>} />
+          </Routes>
+        </BrowserRouter>
       </ThemeProvider>
     </div>
   );
