645a9ed7b9016bed09d2c97c908050cbd7ca9f98
George Varghese M <george@georgevm.com>
2022-08-27 22:17:39 +0530

    Add loader for channel list

--- apps/web/src/archive/channel/components/channel-picker.tsx
+++ apps/web/src/archive/channel/components/channel-picker.tsx
@@ -7,6 +7,7 @@ import {
   ListItemText,
   Typography,
   SxProps,
+  Skeleton,
 } from '@mui/material';
 import { Channel } from '../channel';
 import { useChannels } from '../hooks';
@@ -20,7 +21,7 @@ export const ChannelPicker: React.FC<ChannelPickerProps> = ({
   selectedChannelId,
   onChange = () => {},
 }) => {
-  const { channels } = useChannels();
+  const { channels, loading } = useChannels();
 
   const onChannelClick = (channel: Channel) => {
     onChange(channel);
@@ -28,28 +29,34 @@ export const ChannelPicker: React.FC<ChannelPickerProps> = ({
 
   return (
     <List>
-      {(channels || []).map(channel => (
-        <ChannelListItem
-          channel={channel}
-          selected={selectedChannelId === channel.id}
-          key={channel.id}
-          onClick={() => onChannelClick(channel)}
-        />
-      ))}
+      {channels &&
+        !loading &&
+        channels.map(channel => (
+          <ChannelListItem
+            channel={channel}
+            selected={selectedChannelId === channel.id}
+            key={channel.id}
+            onClick={() => onChannelClick(channel)}
+          />
+        ))}
+      {loading && Array(10).fill(<ChannelListItem />)}
     </List>
   );
 };
 
 const ChannelListItem: React.FC<{
-  channel: Channel;
-  selected: boolean;
+  channel?: Channel;
+  selected?: boolean;
   onClick?: ListItemButtonProps['onClick'];
-}> = ({ channel, selected, onClick }) => {
+}> = ({ channel, selected = false, onClick }) => {
   return (
     <ListItem disablePadding sx={{ marginBottom: 1 }}>
       <ListItemButton onClick={onClick} sx={channelSx(selected)}>
         <ChannelListIcon />
-        <ListItemText primary={channel.name} />
+        {channel && <ListItemText primary={channel.name} />}
+        {!channel && (
+          <Skeleton variant="text" sx={{ fontSize: '1rem' }} width={160} />
+        )}
       </ListItemButton>
     </ListItem>
   );
