cf44bfaafd4e3678893621349c55b9b9ab3bf272
George Varghese M <george@georgevm.com>
2022-11-17 11:35:39 +0530

    Optionally serve static web files from server

--- apps/server/src/server/express/app.js
+++ apps/server/src/server/express/app.js
@@ -1,9 +1,10 @@
 const api = require('../../api')
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 const express = require('express');
 const { authMiddleware, contextMiddleware, delayMiddleware, errorMiddleware } = require('./middleware');
-const { toRequest, sendResponse } = require('./util')
+const { toRequest, sendResponse } = require('./util');
 
 const routeHandler = handlerFn => async (req, res, next) => {
     try {
@@ -35,7 +36,7 @@ const init = async(context) => {
         app.use(delayMiddleware(process.env.TEST_API_DELAY))
     }
 
-    app.get('/', (req, res) => res.send('up'));
+    app.get('/health', (req, res) => res.send('up'));
 
     app.get('/v1/login/auth-url', routeHandler(api['GET:/v1/login/auth-url']))
 
@@ -45,23 +46,32 @@ const init = async(context) => {
 
     app.delete('/v1/login', routeHandler(api['DELETE:/v1/login']));
 
-    app.use(authMiddleware);
+    const protectedRouter = express.Router();
 
-    app.get('/v1/backups/stats', routeHandler(api['GET:/v1/backups/stats']));
+    protectedRouter.use(authMiddleware)
 
-    app.post('/v1/backups/new', routeHandler(api['POST:/v1/backups/new']));
+    protectedRouter.get('/backups/stats', routeHandler(api['GET:/v1/backups/stats']));
 
-    app.get('/v1/backups/running', routeHandler(api['GET:/v1/backups/running']));
+    protectedRouter.post('/backups/new', routeHandler(api['POST:/v1/backups/new']));
 
-    app.get('/v1/backups/:id', routeHandler(api['GET:/v1/backups/:id']));
+    protectedRouter.get('/backups/running', routeHandler(api['GET:/v1/backups/running']));
 
-    app.post('/v1/backups/:id/cancel', routeHandler(api['POST:/v1/backups/:id/cancel']));
+    protectedRouter.get('/backups/:id', routeHandler(api['GET:/v1/backups/:id']));
 
-    app.get('/v1/conversations', routeHandler(api['GET:/v1/conversations']));
+    protectedRouter.post('/backups/:id/cancel', routeHandler(api['POST:/v1/backups/:id/cancel']));
 
-    app.get('/v1/members/:id', routeHandler(api['GET:/v1/members/:id']));
+    protectedRouter.get('/conversations', routeHandler(api['GET:/v1/conversations']));
 
-    app.get('/v1/messages', routeHandler(api['GET:/v1/messages']));
+    protectedRouter.get('/members/:id', routeHandler(api['GET:/v1/members/:id']));
+
+    protectedRouter.get('/messages', routeHandler(api['GET:/v1/messages']));
+
+    app.use('/v1', protectedRouter);
+
+    if (process.env.STATIC_WEB_DIR) {
+        app.use(express.static(process.env.STATIC_WEB_DIR));
+        app.get('*', (req, res) => res.sendFile(path.resolve(process.env.STATIC_WEB_DIR, 'index.html')))
+    }
 
     return app
 }
